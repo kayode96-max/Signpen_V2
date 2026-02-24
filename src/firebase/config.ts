99f7dac439930552bcbb7272779da3a34f0bdd90
Kayode96-max <olukayodeusman406@gmail.com>
2026-02-24 17:14:30 +0100

    chore: add Firebase configuration and environment example

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-8673642774-5bb3f",
-  "appId": "1:312593084520:web:61ce82d480595f96558bdd",
-  "storageBucket": "studio-8673642774-5bb3f.appspot.com",
-  "apiKey": "AIzaSyAdKxdDjy0rxcgEEUk7Ptxwes3IGFtB6Fo",
-  "authDomain": "studio-8673642774-5bb3f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "312593084520"
+  apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId:         process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId:             process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId:     process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? '',
 };
