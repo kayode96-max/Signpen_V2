4081c660c35d26004e6a67e653b51d531d5398e7
Kayode96-max <olukayodeusman406@gmail.com>
2026-02-24 17:06:00 +0100

    chore: add src/firebase/config.ts

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  "projectId": "studio-8673642774-5bb3f",
+  "appId": "1:312593084520:web:61ce82d480595f96558bdd",
+  "storageBucket": "studio-8673642774-5bb3f.appspot.com",
+  "apiKey": "AIzaSyAdKxdDjy0rxcgEEUk7Ptxwes3IGFtB6Fo",
+  "authDomain": "studio-8673642774-5bb3f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "312593084520"
+};
